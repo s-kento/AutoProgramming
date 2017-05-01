6085a716569e0445ac1bfb71f6b7c32ffef72703
s-kento <s-kento@ist.osaka-u.ac.jp>
2017-05-01 11:07:42 +0900

    ファイルの絶対パスを出力するよう変更

--- src/analysis/Main.java
+++ src/analysis/Main.java
@@ -1,18 +1,16 @@
 package analysis;
 
 import java.io.IOException;
-
 import org.eclipse.jdt.core.dom.CompilationUnit;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		SourceFileAnalyzer sfa = new SourceFileAnalyzer(args[0]);
 		for (String filePath : sfa.getFileList()) {
+			System.out.println(filePath);
 			CompilationUnit unit = sfa.getAST(filePath);
 			MyVisitor visitor = new MyVisitor(unit);
 			unit.accept(visitor);
-			System.out.println(visitor.getMethodName());
-			System.out.println(visitor.getReturnType());
 		}
 	}
 }
