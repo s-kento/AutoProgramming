b3e3563ea285ef3bf793a63162aea781aee26137
s-kento <s-kento@ist.osaka-u.ac.jp>
2017-05-01 19:27:18 +0900

    データベースの一覧を表示する処理を追加

--- src/db/Main.java
+++ src/db/Main.java
@@ -2,9 +2,14 @@ package db;
 
 import java.sql.SQLException;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.ParseException;
+
 public class Main {
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		SQLite db= new SQLite("hoge.db");
-		db.test();
+	public static void main(String[] args) throws ClassNotFoundException, SQLException, ParseException {
+		SQLite db= new SQLite("autoprog.db");
+		Config conf = new Config();
+		CommandLine cl = conf.getOptions(args);
+		db.showTable(cl);
 	}
 }
--- src/db/SQLite.java
+++ src/db/SQLite.java
@@ -5,10 +5,17 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Arrays;
 import java.util.List;
+
+import org.apache.commons.cli.CommandLine;
 
 import analysis.MyVisitor;
 
+/*
+ * DBに接続するクラス
+ * @author s-kento
+ */
 public class SQLite {
 
 	Connection connection = null;
@@ -16,7 +23,7 @@ public class SQLite {
 	String db;
 
 	public SQLite(String db) throws ClassNotFoundException, SQLException {// コンストラクタ
-		this.db=db;
+		this.db = db;
 	}
 
 	/*
@@ -24,22 +31,26 @@ public class SQLite {
 	 *
 	 * @param visitor
 	 */
-	public void register(MyVisitor visitor) throws SQLException, ClassNotFoundException{
+	public void register(MyVisitor visitor) throws SQLException, ClassNotFoundException {
 		Class.forName("org.sqlite.JDBC");
 		connection = DriverManager.getConnection("jdbc:sqlite:sqlite/" + db);
-		List<String> parameterType = visitor.getParameterType();
 		statement = connection.createStatement();
-		String sql="insert into method values(\'"+visitor.getFilePath()+"\',\'"+visitor.getClassName()
-		+"\',\'"+visitor.getMethodName()+"\',\'"+visitor.getReturnType()+"\',\'";
-		String params="";
-		for (int i = 0; i <parameterType.size(); i++) {
-			params+=parameterType.get(i);
+		List<String> parameterType = visitor.getParameterType();
+
+		/* SQL文の作成・ここから */
+		String sql = "insert into method values(\'" + visitor.getFilePath() + "\',\'" + visitor.getClassName() + "\',\'"
+				+ visitor.getMethodName() + "\',\'" + visitor.getReturnType() + "\',\'";
+		String params = "";
+		for (int i = 0; i < parameterType.size(); i++) {
+			params += parameterType.get(i);
 			if (i < parameterType.size() - 1) {
-				params+=",";
+				params += ",";
 			}
 		}
-		sql+=params+"\')";
-		statement.executeUpdate(sql);
+		sql += params + "\')";
+		/* SQL文の作成・ここまで */
+
+		statement.executeUpdate(sql);// SQL文の実行
 
 		try {
 			if (statement != null) {
@@ -57,31 +68,65 @@ public class SQLite {
 		}
 	}
 
-	public void test() {
+	/*
+	 * クエリを満たすテーブル一覧を出力
+	 *
+	 * @param cli クエリ
+	 */
+	public void showTable(CommandLine cl) throws ClassNotFoundException, SQLException {
+		Class.forName("org.sqlite.JDBC");
+		connection = DriverManager.getConnection("jdbc:sqlite:sqlite/" + db);
+		statement = connection.createStatement();
+
+		String sql = "select * from method where 1=1";
+		if (cl.hasOption("f")) {
+			sql += " and filepath=\'" + cl.getOptionValue("f") + "\'";
+		}
+		if (cl.hasOption("c")) {
+			sql += " and classname=\'" + cl.getOptionValue("c") + "\'";
+		}
+		if (cl.hasOption("m")) {
+			sql += " and methodname=\'" + cl.getOptionValue("m") + "\'";
+		}
+		if (cl.hasOption("r")) {
+			sql += " and returntype=\'" + cl.getOptionValue("r") + "\'";
+		}
+		if (cl.hasOption("p")) {
+			sql += " and parametertype=\'";
+			String[] parameterType = cl.getOptionValues("p");
+			Arrays.sort(parameterType);
+			for (int i = 0; i < parameterType.length; i++) {
+				sql += parameterType[i];
+				if (i < parameterType.length - 1) {
+					sql += ",";
+				}
+			}
+			sql += "\'";
+		}
+		ResultSet rs = statement.executeQuery(sql);
+		while (rs.next()) {
+			for (int i = 1; i <= 5; i++) {
+				System.out.print(rs.getString(i));
+				if (i < 5)
+					System.out.print(",");
+			}
+			System.out.println();
+		}
+
 		try {
-			statement = connection.createStatement();
-			String sql = "select * from fruits";
-			ResultSet rs = statement.executeQuery(sql);
-			while (rs.next()) {
-				System.out.println(rs.getString(1));
+			if (statement != null) {
+				statement.close();
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
-		} finally {
-			try {
-				if (statement != null) {
-					statement.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
+		}
+		try {
+			if (connection != null) {
+				connection.close();
 			}
-			try {
-				if (connection != null) {
-					connection.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
 	}
+
 }
