e99c0dfe8393be009b4efdb5420f5d16108263a3
s-kento <s-kento@ist.osaka-u.ac.jp>
2017-09-06 15:31:57 +0900

    新規作成

---
+++ src/transformation/GenProg.java
@@ -0,0 +1,10 @@
+package transformation;
+
+import fr.inria.main.evolution.*;
+
+public class GenProg {
+	public static void main(String args[]) throws Exception {
+		AstorMain astor = new AstorMain();
+		astor.execute(args);
+	}
+}
